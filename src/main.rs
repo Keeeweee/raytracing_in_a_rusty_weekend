96a4abfbb6d5ae471fe47b09aca314113d251537
Keeeweee <epsilonmajorquezero@gmail.com>
2023-04-22 02:32:56 +0200

    Generate an image simulating a camera with the color function

--- src/main.rs
+++ src/main.rs
@@ -21,10 +21,17 @@ fn color(ray: Ray) -> Vec3 {
 
 fn main() {
     print_header();
-    
+    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
+    let horizontal = Vec3::new(4.0, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, 2.0, 0.0);
+    let origin = Vec3::new(0.0, 0.0, 0.0);
     for j in (0..NY).rev() {
         for i in 0..NX {
-            let mut col: Vec3 = Vec3::new(i as f64 / NX as f64, j as f64 / NY as f64, 0.2 as f64);
+
+            let u = i as f64 / NX as f64;
+            let v = j as f64 / NY as f64;
+            let ray = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
+            let mut col = color(ray);
             col *= 255.99;
             col.print_as_int();
         }
