e769cf2787c5208d0f02a5fddd637fa8f4840603
Keeeweee <epsilonmajorquezero@gmail.com>
2023-04-23 03:15:51 +0200

    Fix bug in the Sphere hit implementation formula

--- src/hittable.rs
+++ src/hittable.rs
@@ -32,9 +32,9 @@ impl Hittable for Sphere {
     fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
         let oc = ray.origin - self.center;
         let a = ray.direction.dot(ray.direction);
-        let b = 2.0 * ray.direction.dot(oc);
+        let b = ray.direction.dot(oc);
         let c = oc.dot(oc) - self.radius * self.radius;
-        let discriminant = b * b - 4.0 * a * c;
+        let discriminant = b * b - a * c;
 
         if discriminant > 0.0 {
             let temp = (-b - discriminant.sqrt()) / a;
