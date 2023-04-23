3ac64672f83f1aa3d2409d0c911437b86cb63d7a
Keeeweee <epsilonmajorquezero@gmail.com>
2023-04-23 03:26:08 +0200

    Implement /= f64 function for Vec3

--- src/vec3.rs
+++ src/vec3.rs
@@ -134,6 +134,14 @@ impl ops::Div<f64> for Vec3 {
     }
 }
 
+impl ops::DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, f: f64) {
+        self.x /= f;
+        self.y /= f;
+        self.z /= f;
+    }
+}
+
 impl ops::Neg for Vec3 {
     type Output = Vec3;
 
