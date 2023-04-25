b3ed00c498f402c878f8e1622b57011c433d7d27
Keeeweee <epsilonmajorquezero@gmail.com>
2023-04-25 03:15:48 +0200

    Add new options for the Camera position

--- src/camera.rs
+++ src/camera.rs
@@ -1,5 +1,6 @@
 use crate::ray::Ray;
 use crate::vec3::Vec3;
+use std::f64::consts::PI;
 
 pub struct Camera {
     pub lower_left_corner: Vec3,
@@ -18,7 +19,21 @@ impl Camera {
         }
     }
 
+    pub fn new(look_from: Vec3, look_at: Vec3, vup: Vec3, vfov: f64, aspect: f64) -> Camera {
+        let theta = vfov * PI / 180.0;
+        let half_height = (theta / 2.0).tan();
+        let half_width = aspect * half_height;
+        let w = (look_from - look_at).unit();
+        let u = vup.cross(w).unit();
+        let v = w.cross(u);
+        let origin = look_from;
+        let lower_left_corner = origin - half_width * u - half_height * v - w;
+        let horizontal = 2.0 * half_width * u;
+        let vertical = 2.0 * half_height * v;
+        Camera { lower_left_corner, horizontal, vertical, origin }
+    }
+
     pub fn get_ray(&self, u: f64, v: f64) -> Ray {
-        Ray::new(self.origin, self.lower_left_corner + u * self.horizontal + v * self.vertical)
+        Ray::new(self.origin, self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin)
     }
 }
